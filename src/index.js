218549e69367636f28b9ead1546caf89b3a9c53c
RethikRaj <rethikrajrr@gmail.com>
2025-04-27 06:04:30 +0530

    added routing layer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routers/apiRouter.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }) );
 
 app.get('/ping', (req, res) => {
   res.status(StatusCodes.OK).json({ message: 'pong' });
 });
+
+app.use('/api',apiRouter);
 
 app.listen(PORT, () => {
   console.log('Server is running on port 3000');
